0d2a2ed3ac6f55fcd3dc0b696c02e0be9d256119
josef2001 <72079963+josef2001@users.noreply.github.com>
2020-10-12 19:31:03 +0200

    Add files via upload

---
+++ oop.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once 'UserPepa.php';
+
+$Josef = new JOSEF();
+$Josef->setnameJosef('Josefee');
+
+echo $Josef ->getblabla6();
+echo $Josef ->getblabla7();
+echo $Josef ->getblabla8();
+echo $Josef ->getblabla9();
+echo $Josef ->getblabla10();
+echo var_dump($Josef);
+
+?>
+
+
+
+
